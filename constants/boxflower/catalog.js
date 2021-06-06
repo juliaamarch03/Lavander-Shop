b4711c76a808bd1ed02e8d010d3d68c16bc25fd1
juliaamarch03 <juliaamarch@gmail.com>
2021-06-06 19:47:20 +0300

    - добавлено товари на сторінку "квіти в коробках"

---
+++ constants/boxflower/catalog.js
@@ -0,0 +1,26 @@
+const CATALOG = [
+  {
+    id: '100',
+    name: 'Квіти в коробці 100',
+    img: 'images/goods/boxflower/bf1.JPG',
+    price: 750,
+  },
+  {
+    id: '101',
+    name: 'Квіти в коробці 101',
+    img: 'images/goods/boxflower/bf2.JPG',
+    price: 650,
+  },
+  {
+    id: '102',
+    name: 'Квіти в коробці 102',
+    img: 'images/goods/boxflower/bf3.JPG',
+    price: 400,
+  },
+  {
+    id: '103',
+    name: 'Квіти в коробці 103',
+    img: 'images/goods/boxflower/bf1.JPG',
+    price: 350,
+  },
+];
