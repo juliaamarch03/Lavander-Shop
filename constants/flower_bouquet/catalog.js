2a508a3b282af531a64321022ac1e5c987648c2c
juliaamarch03 <juliaamarch@gmail.com>
2021-06-06 13:19:38 +0300

    - добавила необхудні файли для роботи з каталогом (1)

---
+++ constants/flower_bouquet/catalog.js
@@ -0,0 +1,26 @@
+const CATALOG = [
+  {
+    id: '000',
+    name: 'Букет 000',
+    img: 'images/goods/flower_bouquet/fb1.JPG',
+    price: 600,
+  },
+  {
+    id: '001',
+    name: 'Букет 001',
+    img: 'images/goods/flower_bouquet/fb2.JPG',
+    price: 550,
+  },
+  {
+    id: '002',
+    name: 'Букет 002',
+    img: 'images/goods/flower_bouquet/fb3.JPG',
+    price: 400,
+  },
+  {
+    id: '003',
+    name: 'Букет 003',
+    img: 'images/goods/flower_bouquet/fb4.JPG',
+    price: 500,
+  },
+];
