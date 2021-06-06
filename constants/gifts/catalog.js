a4a07def8be3d044bd269916dd79196589b327f0
juliaamarch03 <juliaamarch@gmail.com>
2021-06-07 01:12:28 +0300

    - добавила в каталог подарунки

---
+++ constants/gifts/catalog.js
@@ -0,0 +1,33 @@
+const CATALOG = [
+  {
+    id: '200',
+    name: 'Набір 200',
+    img: 'images/goods/gifts/set1.jpg',
+    price: 800,
+  },
+  {
+    id: '201',
+    name: 'Набір 201',
+    img: 'images/goods/gifts/set2.jpg',
+    price: 950,
+  },
+  {
+    id: '202',
+    name: 'Набір 202',
+    img: 'images/goods/gifts/set3.jpg',
+    price: 650,
+  },
+  {
+    id: '203',
+    name: 'Набір 203',
+    img: 'images/goods/gifts/set4.jpg',
+    price: 650,
+  },
+  {
+    id: '204',
+    name: 'Набір 204',
+    img: 'images/goods/gifts/set5.jpg',
+    price: 700,
+  },
+
+];
