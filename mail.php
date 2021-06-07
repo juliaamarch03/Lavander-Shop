359bd8fcf2e658a75ee6d5674719c92cf655abb8
juliaamarch03 <juliaamarch@gmail.com>
2021-06-07 23:58:27 +0300

    - добавила необхідні змінні для створення відправлення

--- mail.php
+++ mail.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
@@ -7,34 +7,44 @@ $mail->CharSet = 'utf-8';
 $name = $_POST['user_name'];
 $phone = $_POST['user_phone'];
 $email = $_POST['user_email'];
+$oblast = $_POST['user_oblast'];
+$city = $_POST['user_city'];
+$numpost = $_POST['user_numpost'];
+
 
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'dzharuzov@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '$dk820&123'; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+$mail->Username = 'lavandershop21@gmail.com'; //Логін від пошти з якої будуть відправлятись листи
+$mail->Password = 'postavte5'; //Пароль від пошти з якої будуть відправлятись листи
+//$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587; // TCP port to connect to / даний порт тільки для .gmail
+$mail->setFrom('lavandershop21@gmail.com'); //Пошта відправника
+$mail->addAddress('lavandershop21@gmail.com');//Пошта отримувача
 
-$mail->setFrom('dzharuzov@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('gomudusu@p33.org');     // Кому будет уходить письмо 
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->smtpConnect(
+    array(
+        "ssl" => array(
+            "verify_peer" => false,
+            "verify_peer_name" => false,
+            "allow_self_signed" => true
+        )
+    )
+);
+
+$mail->addAddress("lavandershop21@gmail.com", "Lav Shop");
+
 $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
+$mail->Subject = 'Нове замовлення!';
+$mail->Body    = 'Замовник<br>ПІБ: ' .$name . '<br>Номер телефону: ' .$phone. '<br>Електронна пошта: ' .$email. '<br>Область: ' .$oblast. '<br>Місто: ' .$city. '<br>Відділення пошти: ' .$numpost;
 $mail->AltBody = '';
 
 if(!$mail->send()) {
     echo 'Error';
 } else {
-    header('location: thank-you.html');
+    header('location: lastp.html');
 }
 ?>
